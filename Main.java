e7c9db77c9a2b1ae0766b890be14a44c7bf2e7ee
balez4464 <balez4464@wrdsb.ca>
2020-10-06 16:14:50 +0000

    created a program for the user to figure out how much they will have in the bank afer years and interest

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program figures out the users intrest rate based on their input
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +11,48 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates Scanner
+    Scanner input = new Scanner(System.in);
+
+    //asks user for their investing amount
+    System.out.println("Please enter a starting balance");
+    double startingBalance = input.nextDouble();
+
+    //asks user for their interest rate
+    System.out.println("Please enter your interest rate");
+    double interest = input.nextDouble();
+    //double for decimal intrest
+    double interestPercentage = interest / 100;
+
+    //asks user for number of years they are going to invest 
+    System.out.println("Please enter the number of years you will invest for");
+    double years = input.nextInt();
+
+    //double for final costs and interest gain
+    double intrestGain = 0;
+    double finalCost = 0;
+    
+    
+
+    //for loop for calculatoing costs
+    for (double yearAccumulator = 1; yearAccumulator <= years; yearAccumulator++){
+      //calculates sum for the first year
+      if(yearAccumulator == 1){
+        //calculates interestgain
+        intrestGain = startingBalance * interestPercentage;
+        //adds interest gain
+        finalCost = startingBalance + intrestGain;
+        //calculates for rest of the years
+      }else{
+        //takes ammount from previous years and calculates new ammount
+        finalCost = (finalCost * interestPercentage) + finalCost;
+      }
+      
+    }
+    //prints final amount
+    System.out.println("Your final balance would be " + "$" + finalCost);
+
+
     
   }
 }
